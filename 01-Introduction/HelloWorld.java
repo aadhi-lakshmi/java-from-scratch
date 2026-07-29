da34e33b3762e5afdb9bd258f50b0037ce376df3
K AADHI LAKSHMI <aadhilakshmi.karuppaian@gmail.com>
2026-07-29 15:47:34 +0530

    Add Hello World and intro notes

---
+++ 01-Introduction/HelloWorld.java
@@ -0,0 +1,12 @@
+// HelloWorld.java
+// Topic: Introduction to Java
+// This is the entry point of every Java program - the main method.
+
+public class HelloWorld 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println("Hello, World!");
+    System.out.println("Starting my Java learning journey.");
+  }
+}
